0cc03904392a547f4232588898cecb97ba5ce09e
Adarsh123-hash <sidharth193555@gmail.com>
2025-06-27 15:03:36 +0530

    Added new endpoint to RoomService

--- src/main/java/com/lowes/controller/TestController.java
+++ src/main/java/com/lowes/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/testing")
 @RequiredArgsConstructor
@@ -14,5 +15,11 @@ public class TestController {
     public String testEndpoint() {
         return "Hello from Home Renovation Planner!";
     }
+@GetMapping("path")
+public String greet() {
+return" hey user welcome back ";
+}
+
+
 
 }
