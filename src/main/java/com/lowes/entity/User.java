bdd7cf6d32a003fb230ae477e54d74addfd38d6a
Adarsh123-hash <sidharth193555@gmail.com>
2025-06-27 14:52:18 +0530

    Update User.java

--- src/main/java/com/lowes/entity/User.java
+++ src/main/java/com/lowes/entity/User.java
@@ -3,7 +3,7 @@ package com.lowes.entity;
 import jakarta.persistence.*;
 import lombok.*;
 @Entity
-@Table(name = "user")  // <--- change table name
+@Table(name = "app_user")  // <--- change table name
 @Getter
 @Setter
 public class User {
